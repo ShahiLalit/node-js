8b513d70b9caf2bcb530423d7376a1e27b81a487
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-09-16 12:17:59 +0530

    Web server checklist

---
+++ webServerChecklist.js
@@ -0,0 +1,30 @@
+var webServerChecklist = {
+  1: "Better ways to organize our code into reusable pieces",
+  2: "Ways to deal with files",
+  3: "Ways to deal with databases",
+  4: "The ability to communicate over the internet",
+  5: "The ability to accept requests and send responses in the standard format",
+  6: "A way to deal with work tha takes a long time",
+};
+
+/*
+ ==> 1: "Better ways to organize our code into reusable pieces"
+
+      Using Modules and Imports
+
+*/
+
+/*
+ ==> 2: "Ways to deal with files",
+
+      Using Streams and Buffers, callbacks, to deal with binary data
+      
+
+*/
+
+/*
+ ==> 6: "A way to deal with work tha takes a long time",
+
+      Synchronously and Asynchronously 
+
+*/
