eb8c956c587d78856faab418494403e2f3ec4933
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-09-16 22:26:44 +0530

    Outputting the html through node server

--- httpServer.js
+++ httpServer.js
@@ -1,10 +1,15 @@
 var http = require("http");
+var fs = require("fs");
 
 http
   .createServer(function(req, res) {
     res.writeHead(200, {
-      "Content-Type": "text/plain",
+      "Content-Type": "text/html",
     });
-    res.end("Hello World\n");
+    var html = fs.readFileSync(`${__dirname}/index.html`, "utf8"); // without utf8 it will in the binary form, adding utf8 converts the data into string.
+
+    var message = "Dynamic Hello World!!";
+    html = html.replace("{message}", message);
+    res.end(html);
   })
   .listen(1337, "127.0.0.1");
