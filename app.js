5a3acaac7d43f415f976dfbc42dba6e9db977a71
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-09-23 18:39:19 +0530

    bodyParser

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+// create application/json parser
+var jsonParser = bodyParser.json();
 
 var port = process.env.PORT || 3000;
 
@@ -20,6 +27,12 @@ app.get("/person/:id", function(req, res) {
   res.render("person", {ID: req.params.id});
 });
 
+app.post("/person", urlencodedParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400);
+  res.send("Thank you" + JSON.stringify(req.body));
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
 app.get("/api", function(req, res) {
   res.json({firstname: "John", lastname: "Carter"});
 });
